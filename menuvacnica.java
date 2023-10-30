2ad4c3d6b4b9848b72e31c17c734d5d1c3a63b2f
iki55 <147849800+iki55@users.noreply.github.com>
2023-10-30 22:22:13 +0100

    Add files via upload

---
+++ menuvacnica.java
@@ -0,0 +1,15 @@
+package proekt1;
+
+public class menuvacnica {
+
+	public static void main(String[] args) {
+	double x=23.5;
+	double evro =61.7;
+	double rezultat=x*evro;
+	System.out.println("rezultatot iznesuva");
+	System.out.println(rezultat);
+	
+
+	}
+
+}
